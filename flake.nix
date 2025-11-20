89707254c4b55136ea61b075cf34d44bb351b6d1
Uberch <vam.molch@gmail.com>
2025-11-20 21:45:34 +0300

    Feat(hm-man): Add initial home manager conf.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,18 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+		
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = {nixpkgs, ...}: {
+	outputs = {nixpkgs, ...}@inputs: {
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
 			modules = [
 				./hosts/default/configuration.nix
+				inputs.home-manager.nixosModules.default
 			];
 		};
 	};
--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -66,6 +66,7 @@
   environment.systemPackages = with pkgs; [
     neovim
     git
+    ranger
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
