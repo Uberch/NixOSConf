364261aead3599b222b051e4406410f40bc7b806
Uberch <vam.molch@gmail.com>
2025-11-21 10:04:54 +0300

    Feat(hm-man): Separate bash config to separate file inside modules

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+	imports = [
+		./modules
+	];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home = {
@@ -75,44 +78,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  programs.bash = {
-  	enable = true;
-  	shellAliases =
-	let
-		flake_path = "~/nix";
-	in {
-		# General purpose
-		v = "$EDITOR";
-		c = "clear";
-		ll = "ls -la";
-		rg = "ranger";
-
-		# Power managment
-		sdn = "systemctl poweroff";
-		rbt = "reboot";
-
-		# Nix-related aliases
-		rebuild = "sudo nixos-rebuild switch --flake ${flake_path}";
-		hman = "home-manager switch ${flake_path}";
-		# Git aliases
-		ga = "git add";
-		gb = "git branch";
-		gc = "git commit";
-		gch = "git checkout";
-		gd = "git diff";
-		gf = "git fetch";
-		gp = "git push";
-		gpl = "git pull";
-		grm = "git rm";
-		grs = "git restore";
-		gs = "git status";
-		gst = "git stash";
-		gsp = "git add .; git statsh; git pull";
-	};
-
-	bashrcExtra = ''
-		set -o vi
-	'';
-  };
 }
