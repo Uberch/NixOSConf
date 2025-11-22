f5e7113ad8ab4961cf2c620745e4d437ef2823e7
Uberch <vam.molch@gmail.com>
2025-11-22 11:13:18 +0300

    Feat(nixvim): Adjust nvim options to initial working state

--- home-manager/modules/neovim/options.nix
+++ home-manager/modules/neovim/options.nix
@@ -1,50 +1,60 @@
 {
-  programs.nixvim = {
-    opts = {
-      # Positioning
-      number = true;
-      relativenumber = true;
-      ruler = true;
-      cursorline = true;
+	programs.nixvim.opts = {
+		# Positioning
+		number = true;						# Enumerate lines
+		relativenumber = true;			# Enumerate lines with respect to cursor
+		cursorline = true;				# Color line on which cursor is placed
 
-      # Indentation
-      autoindent = true;
-      tabstop = 3;
-      softtabstop = 3;
-      shiftwidth = 3;
-      smartab = true;
+		# Indentation
+		autoindent = true;				# Carry indentation when entering new line
+		tabstop = 3;
+		shiftwidth = 3;					# Width of tab
+		shiftround = true;				# Round indentation to several shiftwidth when using < or > commands
 
-      # Mouse (unused for the moment)
-      # scrolloff = 8;
-      # ttyfast = true;
-      # mouse = "a";
-      # mousehide = true;
+		# Copy/Paste
+		clipboard = "unnamedplus";		# Use '+' buffer as default clipboard
 
-      # Copy/Paste
-      # clipboard = unnamedplus;
+		# Search
+		ignorecase = true;
+		smartcase = true;					# Overwrites ignorecase when there are capital letters in searched text
+		incsearch = true;					# Show results for the search text typed so far
+		showmatch = true;
+		hlsearch = true;					# Highlight search results
 
-      # Search
-      showmatch = true;
-      ignorecase = true;
-      incsearch = true;
-      hlsearch = true;
+		# Displaying
+		scrolloff = 8;						# Cap between cursor and top/bottom borders of screen
+		breakindent = true;				# Carry indentation when one line splits on several lines
+		linebreak = true;					# Break line on special chars, not at the moment it does not fit on one line
+		cursorbind = true;				# Binding cursor position in diff mode
+		ruler = true;						# Show cursor position
+		signcolumn = "yes:1";			# Locks the size and presence of signcolumn between line number and text itself
 
-      # updatetime = 100;
-      fileencoding = "utf-8";
-      breakindent = true;
-      undofile = true;
-      signcolumn = "yes";
-      timeoutlen = 300;
-      splitbelow = true;
-      splitright = true;
-      list = true;
-      # listchars = {
-      #   tab = "▸ ";
-      #   trail = "·";
-      #   eol = "↵";
-      #   space = "·";
-      # };
-      inccommand = "split";
-    };
-  };
+		# Mouse (unused)
+		# ttyfast = true;
+		# mouse = "a";
+		# mousehide = true;
+
+		# Ungrouped
+		fileencoding = "utf-8";			# File encoding, lol
+		undofile = true;					# Saves undo history for file in buffer
+		timeout = true;					# If enabled cancels command after timeoutlen milliseconds
+		timeoutlen = 1000;				# How long (in milliseconds) to wait for next part of unfinished command
+		updatetime = 4000;				# Timeout for swap file saving
+		updatecount = 200;				# Number of characters before saving swap file
+
+		# To play with
+		rulerformat = "";					# Defines format of ruler info
+		statusline = "";
+		smarttab = true;
+		softtabstop = 3;
+		matchpairs = "(:),[:],{:},<:>";
+		list = true;
+		listchars = {
+			trail = "·";
+			lead = "·";
+			tab = "   ";
+			# eol = "↵";
+			# space = "·";
+		};
+	};
 }
