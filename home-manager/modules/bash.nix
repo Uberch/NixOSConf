5aee3b6bd1616e6845881745ce41a0445bdaf20c
Uberch <vam.molch@gmail.com>
2025-11-26 10:09:59 +0300

    Format(hm-man): Format configuration

--- home-manager/modules/bash.nix
+++ home-manager/modules/bash.nix
@@ -1,31 +1,31 @@
 {
-  programs.bash = {
-  	enable = true;
-  	shellAliases =
-	let
-		flake_path = "~/nix";
-	in {
-		# General purpose
-		v = "$EDITOR";
-		c = "clear";
-		ll = "ls -la";
-		rg = "ranger";
+	programs.bash = {
+		enable = true;
+		shellAliases =
+		let
+			flake_path = "~/nix";
+		in {
+			# General purpose
+			v = "$EDITOR";
+			c = "clear";
+			ll = "ls -la";
+			rg = "ranger";
 
-		# Power managment
-		sdn = "systemctl poweroff";
-		rbt = "reboot";
+			# Power managment
+			sdn = "systemctl poweroff";
+			rbt = "reboot";
 
-		# Nix-related aliases
-		rebuild = "sudo nixos-rebuild switch --flake ${flake_path}";
-		hman = "home-manager switch --flake ${flake_path}";
-		
-		# Git aliases
-		g = "git";
-		gsp = "git add .; git statsh; git pull";
+			# Nix-related aliases
+			rebuild = "sudo nixos-rebuild switch --flake ${flake_path}";
+			hman = "home-manager switch --flake ${flake_path}";
+			
+			# Git aliases
+			g = "git";
+			gsp = "git add .; git statsh; git pull";
+		};
+
+		bashrcExtra = ''
+			set -o vi
+		'';
 	};
-
-	bashrcExtra = ''
-		set -o vi
-	'';
-  };
 }
