581806dab21d0c5801dc1e9f2ddf00b95b15484f
Uberch <vam.molch@gmail.com>
2026-01-10 14:26:57 +0300

    Upd(general): Move to 25.11 version of almost all flake inputs

--- users/modules/ssh.nix
+++ users/modules/ssh.nix
@@ -2,23 +2,29 @@
 	programs.ssh = {
 		enable = true;
 
-		addKeysToAgent = "yes";		# Add key identities to key agent
-		hashKnownHosts = true;		# Hash stored known hosts
+		enableDefaultConfig = false;
+
 		matchBlocks = {
 			gh = {
 				user = "git";
 				hostname = "github.com";
 				identityFile = "/home/uber/.ssh/keys/gh/key";
+				hashKnownHosts = true;		# Hash stored known hosts
+				addKeysToAgent = "yes";		# Add key identities to key agent
 			};
 			ghiu = {
 				user = "git";
 				hostname = "github.com";
 				identityFile = "/home/uber/.ssh/keys/ghiu/key";
+				hashKnownHosts = true;		# Hash stored known hosts
+				addKeysToAgent = "yes";		# Add key identities to key agent
 			};
 			gliu = {
 				user = "git";
 				hostname = "gitlab.pg.innopolis.university";
 				identityFile = "/home/uber/.ssh/keys/gliu/key";
+				hashKnownHosts = true;		# Hash stored known hosts
+				addKeysToAgent = "yes";		# Add key identities to key agent
 			};
 		};
 	};
