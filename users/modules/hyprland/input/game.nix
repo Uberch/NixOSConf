9dabae7720d19deaa189a1e6f18e8ea80c0e1bff
Uberch <vam.molch@gmail.com>
2026-01-10 22:16:57 +0300

    Fix(hypr): Fix device config placement

--- users/modules/hyprland/input/game.nix
+++ users/modules/hyprland/input/game.nix
@@ -1,10 +1,12 @@
 { lib, config, ... }: {
 config = lib.mkIf (config.hyprland.enable && config.hyprland.inputMode == "game") {
-	wayland.windowManager.hyprland.settings.input = {
-		follow_mouse = 1;
-		mouse_refocus = true;
-		focus_on_close = 1;
-		touchpad.disable_while_typing = false;
+	wayland.windowManager.hyprland.settings={
+		input = {
+			follow_mouse = 1;
+			mouse_refocus = true;
+			focus_on_close = 1;
+			touchpad.disable_while_typing = false;
+		};
 		device = [
 			{
 				name = "elan260b:00-04f3:31dc-touchpad";
